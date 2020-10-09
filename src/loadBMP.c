a9f2946d6fde8e52c4a61aa1c21ea41ac48f8077
Loic Segundo <loic.segundo@epita.fr>
2020-10-09 20:48:23 +0000

    Getpixel is now implemented but not understood

--- src/loadBMP.c
+++ src/loadBMP.c
@@ -33,6 +33,42 @@ int displayBMP(char *path)
     return EXIT_SUCCESS;
 }
 
+Uint32 getpixel(SDL_Surface *image, int x, int y)
+{
+    int pixel_bytes = image->format->BytesPerPixel;
+    // Here pixel is the address to the pixel we want to retrieve
+    Uint8 *pixel = (Uint8 *)image->pixels + y * image->pitch + x * pixel_bytes;
+
+    switch (pixel_bytes)
+    {
+        case 1:
+            return *pixel;
+            break;
+
+        case 2:
+            return *(Uint16 *)pixel;
+            break;
+
+        case 3:
+            // If most significant byte is pixel[0] shift it.
+            if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
+                // Return one Uint32 due to bitwise or
+                return pixel[0] << 16 | pixel[1] << 8 | pixel[2];
+            else
+                return pixel[0] | pixel[1] << 8 | pixel[2] << 16;
+            break;
+
+        case 4:
+            return *(Uint32 *)pixel;
+            break;
+
+        default:
+            // To avoid warnings
+            return 0;
+    }
+}
+
+
 int hello ()
 {
     printf ("Hello World\n");
