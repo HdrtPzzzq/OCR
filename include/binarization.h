aa9a246742b2d6e552416a5d6994cb44226ad838
Raffaël MORAISIN <raffael.moraisin@epita.fr>
2020-10-19 18:37:59 +0200

    comments in grey_level.c and binarization.h

--- include/binarization.h
+++ include/binarization.h
@@ -2,7 +2,7 @@
 #define BINARIZATION_H
 
 #include<SDL/SDL.h>
-Uint32 binarization_pixel(Uint32 pixel,SDL_PixelFormat *fmt, int seuil);
-void binarization(SDL_Surface *surface, SDL_PixelFormat *fmt, int seuil);
+Uint32 binarization_pixel(Uint32 pixel,SDL_PixelFormat *fmt, int threshold);
+void binarization(SDL_Surface *surface, SDL_PixelFormat *fmt, int threshold);
 
 #endif
