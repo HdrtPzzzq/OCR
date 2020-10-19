ab6d04e7ca07f3c4ffa36a8d74e498591452da7b
Raffaël MORAISIN <raffael.moraisin@epita.fr>
2020-10-19 15:05:30 +0200

    Modification of some comments in src/grey_level.c

--- src/grey_level.c
+++ src/grey_level.c
@@ -2,10 +2,11 @@
 
 Uint32 grey_level(Uint32 pixel)
 {
-    //create a color where we gonna stock the g value, b value and r value 
+    //create a color where we gonna stock the g value, b value and r value of
+    //the pixel in th input. 
     SDL_Color color;
     SDL_GetRBG(pixel,format,&color.r,&color.g,&color.b);
     //get the level of grey of the pixel by the next formule using the
-    //componants of color
+    //componants of color.
     return (Uint32)(color.r * 0,2126 + color.v * 0,7152 + color.b * 0,0722);
 }
